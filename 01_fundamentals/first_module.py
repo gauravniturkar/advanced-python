08e47ce90a9d79740b5637ecfa9ad1af1d63509b
gauravniturkar <niturkar.gaurav8502@gmail.com>
2026-01-21 20:40:49 -0800

    basic python module handeling

---
+++ 01_fundamentals/first_module.py
@@ -0,0 +1,8 @@
+
+print("Executing First Module")
+
+
+if __name__ == '__main__': 
+    print("First Module Name:", __name__)
+else:
+    print("First Module Imported:", __name__)
---
+++ 01_fundamentals/second_module.py
@@ -0,0 +1,3 @@
+import first_module
+
+print("Second Module Name:", __name__)
